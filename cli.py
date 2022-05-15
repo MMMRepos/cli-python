10e349e16d640eef90ccb80de8b5cfaa8ee9a247
Madhura Mujumdar <47087434+MMMRepos@users.noreply.github.com>
2022-05-15 08:14:41 -0700

    Also added logic for performing an action based on the initial menu selection.

--- cli.py
+++ cli.py
@@ -1,10 +1,15 @@
 from collections import defaultdict
 from comms import CommunicationAdapter
+from command import CommandFactory
 from keyLogger import KeyLogger
 
 def main():
     commLayer = CommunicationAdapter()
     keyHistory = KeyLogger()
+    cliCommandFactory = CommandFactory()
+    cliCommandFactory.registerCommand("Hello", "Hello, it is nice to meet you.")
+    cliCommandFactory.registerCommand("Good Bye", "I need to run; but it has been great talking.")
+    cliCommandFactory.registerCommand("Hold Please", "Excuse me a moment, someone else is here.")
     print("CLI Task")
     print("\n*** Main Menu ***\n")
     menuDict = defaultdict(str)
@@ -13,7 +18,7 @@ def main():
     menuDict["3"] = "Update Device"
     menuDict["4"] = "Close"
     for key, value in menuDict.items():
-        print("    " + key + ". " + value)
+        print("    " + key + ". " + str(value))
     print("\n")
     commLayer.displayCommPorts()
     portNumber = input("Select the port from the available COM ports ")
@@ -23,8 +28,18 @@ def main():
     
     while True:
         menuSelection = input("Enter your selection number ")
-        
         print("Selected option", menuSelection)
+        if menuSelection == "1":
+            cliCommandFactory.displayCommands()
+        elif menuSelection == "2":
+            print("Running tests")
+        elif menuSelection == "3":
+            print("Update device")
+        elif menuSelection == "4":
+            print("Close")
+            
+        
+        
         keyHistory.appendLastKey(menuDict[str(menuSelection)])
         keyHistory.printKeyStack()
     
