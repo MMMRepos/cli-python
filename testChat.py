e06d12fe917ce9e69268a576a8ad6c9cdacb115e
Madhura Mujumdar <47087434+MMMRepos@users.noreply.github.com>
2022-05-15 10:59:57 -0700

    Added On Hold and Extended Call tests. Also added formatting for printing the test commands and response on the CLI.

--- testChat.py
+++ testChat.py
@@ -23,14 +23,42 @@ class TestChat:
     
     def runTest(self):
         transmitBuffer = []
-        print(len(CLITest.QUICK_CHAT.testSequence))
+        print("    ", end = "")
+        # print(len(CLITest.QUICK_CHAT.testSequence))
         for i in range(len(CLITest.QUICK_CHAT.testSequence)):
-            print(CLITest.QUICK_CHAT.testSequence[i])
+            print(CLITest.QUICK_CHAT.testSequence[i].commandKey.strip("\n"), end = " ")
         # for command in CLITest.QUICK_CHAT.testSequence:
             if CLITest.QUICK_CHAT.testSequence[i] != Command.TERMINATOR:
                 transmitBuffer.append(CLITest.QUICK_CHAT.testSequence[i])
             else:
+                print("\n    Response", end = " " )
                 print(self.transmitCommands(transmitBuffer))
+                transmitBuffer = []
+    
+    def runOnHoldTest(self):
+        transmitBuffer = []
+        print("    ", end = "")
+        for i in range(len(CLITest.ON_HOLD.testSequence)):
+            print(CLITest.ON_HOLD.testSequence[i].commandKey.strip("\n"), end = " ")
+            if CLITest.ON_HOLD.testSequence[i] != Command.TERMINATOR:
+                transmitBuffer.append(CLITest.ON_HOLD.testSequence[i])
+            else:
+                print("\n    Response ", end = " " )
+                print(self.transmitCommands(transmitBuffer))
+                print("    ", end = "")
+                transmitBuffer = []
+    
+    def runExtendedCallTest(self):
+        transmitBuffer = []
+        print("    ", end = "")
+        for i in range(len(CLITest.EXTENDED_CALL.testSequence)):
+            print(CLITest.EXTENDED_CALL.testSequence[i].commandKey.strip("\n"), end = " ")
+            if CLITest.EXTENDED_CALL.testSequence[i] != Command.TERMINATOR:
+                transmitBuffer.append(CLITest.EXTENDED_CALL.testSequence[i])
+            else:
+                print("\n    Response ", end = " " )
+                print(self.transmitCommands(transmitBuffer))
+                print("    ", end = "")
                 transmitBuffer = []
     
     def transmitCommands(self, transmitBuffer):
@@ -38,6 +66,18 @@ class TestChat:
         for command in transmitBuffer:
             receiveBuffer.append(self.commLayer.exchangeData(command.message + LINE_FEED + CARRIAGE_RETURN))
         return receiveBuffer
+    
+    def runTests(self):
+        print("\n*** Running tests ***\n")
+        print("    Test 1: Quick Chat")
+        self.runTest()
+        print("")
+        print("    Test 2: Extended Call")
+        self.runExtendedCallTest()
+        print("")
+        print("    Test 3: On Hold")
+        self.runOnHoldTest()
+        print("")
             
         
     
