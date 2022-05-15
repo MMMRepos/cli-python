e5188d527e4e1c641ff8aa2f39a659308201e009
Madhura Mujumdar <47087434+MMMRepos@users.noreply.github.com>
2022-05-15 08:09:30 -0700

    Added a command factory for the Hello, Good Bye and Hold Please commands.

---
+++ command.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+LINE_FEED = "\n"
+
+class CommandFactory:
+    def __init__(self):
+        self.commands = defaultdict(str)
+        
+    def registerCommand(self, commandKey, commandValue):
+        self.commands[commandKey] = commandValue
+    
+    def getCommandValue(self, commandKey):
+        value = self.commands[commandKey] 
+        if value == "":
+            return None
+        else:
+            return value + LINE_FEED
+        
+    def displayCommands(self):
+        print("\n*** List of Commands ***\n")
+        for key, value in self.commands.items():
+            print("    " + key + ": " + value)
+        print("\n")
+        # print(dict(self.commands))
