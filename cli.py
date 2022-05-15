4f0403996194c0194199bfab3acdc7192cb3a999
Madhura Mujumdar <47087434+MMMRepos@users.noreply.github.com>
2022-05-15 09:48:06 -0700

    Moved the updateDevice() method to the DeviceFirmwareUpdate class. Also moved the firmware image here.

--- cli.py
+++ cli.py
@@ -5,11 +5,6 @@ from keyLogger import KeyLogger
 from dfu import DeviceFirmwareUpdate
 import sys
 
-firmwareImage = [0xFA, 0xCF, 0x04, 0xF0, 0xFB, 0xCF, 0x05, 0xF0, 0xE9, 0xCF, 0x06, 0xF0, 0xEA, 0xCF, 0x07, 0xF0,
-                 0xE1, 0xCF, 0x08, 0xF0, 0xE2, 0xCF, 0x09, 0xF0, 0xD9, 0xCF, 0x0A, 0xF0, 0xDA, 0xCF, 0x0B, 0xF0,
-                 0xF3, 0xCF, 0x0C, 0xF0, 0xF4, 0xCF, 0x0D, 0xF0, 0xF6, 0xCF, 0x0E, 0xF0, 0xF7, 0xCF, 0x0F, 0xF0,
-                 0xF8, 0xCF, 0x10, 0xF0, 0xF5, 0xCF, 0x11, 0xF0, 0xD7, 0x68, 0xD6, 0x68, 0xF2, 0x94, 0x9E, 0x96]
-
 def main():
     commLayer = CommunicationAdapter()
     keyHistory = KeyLogger()
@@ -65,16 +60,7 @@ def main():
             print("Running tests")
         elif menuSelection == "3":
             print("Update device")
-            for i in range(0, len(firmwareImage), 16):
-                packet = []
-                for j in range(i, i+16):
-                    packet.append(firmwareImage[j])
-                crcResult = bootload.calculateCRC16(packet)
-                print("CRC result ", hex(crcResult))
-                packet.append(crcResult & 0x00FF)
-                packet.append(crcResult >> 8)
-                commandResponse = commLayer.executeCommand(packet)
-                print("Command response ", commandResponse)
+            
         elif menuSelection == "4":
             print("Key Logger History")
             keyHistory.printKeyQueue()
