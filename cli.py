f98f8a447212127160e6af77e943b38c52848ab8
Madhura Mujumdar <47087434+MMMRepos@users.noreply.github.com>
2022-05-14 23:23:29 -0700

    Added a basic menu for navigation of options.

--- cli.py
+++ cli.py
@@ -1,8 +1,19 @@
+from collections import defaultdict
 from comms import CommunicationAdapter
 
 def main():
     print("CLI Task")
     print("\n*** Main Menu ***\n")
+    menuDict = defaultdict(str)
+    menuDict["1"] = "Send Command"
+    menuDict["2"] = "Run Test"
+    menuDict["3"] = "Update Device"
+    menuDict["4"] = "Close"
+    for key, value in menuDict.items():
+        print("    " + key + ". " + value)
+    print("\n")
+    menuSelection = int(input("Enter your selection number "))
+    print("Selected option", menuSelection)
     commLayer = CommunicationAdapter()
     commLayer.displayCommPorts()
     portNumber = input("Select the port from the available COM ports ")
