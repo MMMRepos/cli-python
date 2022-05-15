cb7f53c74fb5d4b8dae7fc49049fc005d8ce7984
Madhura Mujumdar <47087434+MMMRepos@users.noreply.github.com>
2022-05-15 10:05:57 -0700

    Refactored the CLI for separation of responsibilities.

--- cli.py
+++ cli.py
@@ -1,74 +1,82 @@
 from collections import defaultdict
 from comms import CommunicationAdapter
-from command import CommandFactory
+from command import Command
 from keyLogger import KeyLogger
 from dfu import DeviceFirmwareUpdate
 import sys
+from enum import Enum
+
+
+
+menuDict = defaultdict(str)
 
 def main():
-    commLayer = CommunicationAdapter()
     keyHistory = KeyLogger()
-    cliCommandFactory = CommandFactory()
-    bootload = DeviceFirmwareUpdate()
-    
-    cliCommandFactory.registerCommand("Hello", "Hello, it is nice to meet you.")
-    cliCommandFactory.registerCommand("Good Bye", "I need to run; but it has been great talking.")
-    cliCommandFactory.registerCommand("Hold Please", "Excuse me a moment, someone else is here.")
-    
-    print("CLI Task")
-    print("\n*** Main Menu ***\n")
-    menuDict = defaultdict(str)
-    menuDict["1"] = "Send Command"
-    menuDict["2"] = "Run Test"
-    menuDict["3"] = "Update Device"
-    menuDict["4"] = "Close"
-    
-    for key, value in menuDict.items():
-        print("    " + key + ". " + str(value))
-    print("\n")
-    
-    commLayer.displayCommPorts()
-    portNumber = input("Select the port from the available COM ports ")
-    baudRate = int(input("Enter the baud rate "))
-    timeOut = int(input("Enter the timeout ")) 
-    commLayer.openComPort(portNumber, baudRate, timeOut)
+    cliCommands = Command()
+    commLayer = initializeCommLayer()
+    bootload = DeviceFirmwareUpdate(commLayer)
+
+    displayMainMenu()
     
     while True:
         menuSelection = input("Enter your selection number ")
         print("Selected option", menuSelection)
         keyHistory.appendLastKey(menuDict[str(menuSelection)])
         
-        if menuSelection == "1":
-            cliCommandFactory.displayCommands()
-            commandInput = input("Enter your choice of command ")
-            # commandList = cliCommandFactory.processInputCommands(commandInput)
-            print(commandInput)
-            commandList = commandInput.split("+")
-            print(commandList)
-            for command in commandList:
-                command = command.strip(" ")
-                print(command)
-                commandValue = cliCommandFactory.getCommandValue(command)
-                if commandValue is not None:
-                    print("Command entered ", command)
-                    print("Message sent ", commandValue)
-                    commandResponse = commLayer.executeCommand(command)
-                    print("Command response ", commandResponse)            
-                else:
-                    print("Invalid command")
-        elif menuSelection == "2":
-            print("Running tests")
-        elif menuSelection == "3":
-            print("Update device")
-            
-        elif menuSelection == "4":
-            print("Key Logger History")
-            keyHistory.printKeyQueue()
-            userInput = input("Press ENTER to close the CLI")
-            print(userInput)
-            print("Thank you for using the CLI")
-            if userInput == "":
-                sys.exit()
+        if menuSelection == CLIMenu.SEND_COMMAND.number:
+            cliCommands.sendCommand(commLayer)  
+        elif menuSelection == CLIMenu.RUN_TEST.number:
+            runTests()
+        elif menuSelection == CLIMenu.UPDATE_DEVICE.number:
+            bootload.updateDevice()
+        elif menuSelection == CLIMenu.CLOSE.number:
+            close(keyHistory)
+
+def close(keyHistory):
+    print("Key Logger History")
+    keyHistory.printKeyQueue()
+    userInput = input("Press ENTER to close the CLI")
+    print(userInput)
+    print("Thank you for using the CLI")
+    if userInput == "":
+        sys.exit()
+
+def runTests():
+    print("Running tests")
+    print("Test 1: Quick Chat")
+    buffer1 = ["Hello", "Good Bye"]
+    print("Test 2: Extended Call")
+    print("Test 1: On Hold")
+
+def displayMainMenu():
+    print("CLI Task")
+    print("\n*** Main Menu ***\n")
+    menuDict[CLIMenu.SEND_COMMAND.number] = CLIMenu.SEND_COMMAND.text
+    menuDict[CLIMenu.RUN_TEST.number] = CLIMenu.RUN_TEST.text
+    menuDict[CLIMenu.UPDATE_DEVICE.number] = CLIMenu.UPDATE_DEVICE.text
+    menuDict[CLIMenu.CLOSE.number] = CLIMenu.CLOSE.text
+    for key, value in menuDict.items():
+        print("    " + key + ". " + str(value))
+    print("\n")
+    
+def initializeCommLayer():
+    commLayer = CommunicationAdapter()
+    commLayer.displayCommPorts()
+    portNumber = input("Select the port from the available COM ports ")
+    baudRate = int(input("Enter the baud rate "))
+    timeOut = int(input("Enter the timeout ")) 
+    commLayer.openComPort(portNumber, baudRate, timeOut)
+    return commLayer
+
+class CLIMenu(Enum):
+    SEND_COMMAND = ("1", "Send Command")
+    RUN_TEST = ("2", "Run Test")
+    UPDATE_DEVICE = ("3", "Update Device")
+    CLOSE = ("4", "Close")
+    def __init__(self, number, text):
+        self.number = number       
+        self.text = text   
+    
         
         
     
