503dccd40c0c171f56390de4088f0201c21feb77
Madhura Mujumdar <47087434+MMMRepos@users.noreply.github.com>
2022-05-15 11:21:22 -0700

    Added documentation for the KeyLogger class and the public methods of the class.

--- keyLogger.py
+++ keyLogger.py
@@ -1,11 +1,21 @@
 from collections import deque
 
-
 class KeyLogger:
+    """This class is used for storing the log of actions performed on using the CLI."""
+    
     def __init__(self):
         self.keyQueue = deque(maxlen = 500)
     
     def printKeyQueue(self):
+        """Print the actions stored in the key logger queue
+        
+        Parameters:
+        None
+
+        Returns:
+        None
+        """
+        
         print(list(self.keyQueue))
         
     def appendLastKey(self, key):
